c26916f7ff1496c1468e452a5a77261cc7402acf
Tiago <tiagodev@protonmail.com>
2022-03-22 16:02:12 -0300

    feat: smooth scroll

--- script.js
+++ script.js
@@ -36,3 +36,30 @@ const initAccordion = () => {
   }
 };
 initAccordion();
+
+const initScroll = () => {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  if (linksInternos.length) {
+    const scrollToSection = (event) => {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+
+      // alternative way
+      // const topo = section.offsetTop;
+      // window.scrollTo({
+      //   top: topo,
+      //   behavior: "smooth",
+      // });
+    };
+
+    linksInternos.forEach((link) =>
+      link.addEventListener("click", scrollToSection)
+    );
+  }
+};
+initScroll();
