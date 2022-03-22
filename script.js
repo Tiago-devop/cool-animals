720530453689f1c45078c39256613bccd4358d5b
Tiago <tiagodev@protonmail.com>
2022-03-22 17:22:00 -0300

    feat: animation scroll

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const initAccordion = () => {
 };
 initAccordion();
 
-const initScroll = () => {
+const initScrollSmooth = () => {
   const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
   if (linksInternos.length) {
     const scrollToSection = (event) => {
@@ -62,4 +62,22 @@ const initScroll = () => {
     );
   }
 };
-initScroll();
+initScrollSmooth();
+
+const initAnimationScroll = () => {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    const animaScroll = () => {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+      });
+    };
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+};
+initAnimationScroll();
