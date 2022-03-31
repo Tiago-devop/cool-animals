299fec63bcd05234b620c34c62a92b659f852e95
Tiago <tiagodev@protonmail.com>
2022-03-31 11:35:36 -0300

    feat: change to modules

---
+++ js/modules/scroll-animation.js
@@ -0,0 +1,18 @@
+const initAnimationScroll = () => {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    const animaScroll = () => {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+      });
+    };
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+};
+
+export default initAnimationScroll;
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSmooth from "./modules/scroll-suave.js";
+import initAnimationScroll from "./modules/scroll-animation.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabNav.js";
+
+initScrollSmooth();
+initAnimationScroll();
+initAccordion();
+initTabNav();
