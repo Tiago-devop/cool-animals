67c1a9e458142d2a556554cea48ad222d98e4c31
Tiago <tiagodev@protonmail.com>
2022-04-07 13:49:36 -0300

    feat: adding tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimationScroll from "./modules/scroll-animation.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabNav.js";
 import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initScrollSmooth();
 initAnimationScroll();
 initAccordion();
 initTabNav();
-initModal()
+initModal();
+initTooltip();
