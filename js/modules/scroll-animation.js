50908202462b56781ab98c97147269d16bbc943c
Tiago <tiagodev@protonmail.com>
2022-04-19 16:25:49 -0300

    feat: add numbers animation

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -8,6 +8,9 @@ const initAnimationScroll = () => {
         const sectionTop = section.getBoundingClientRect().top;
         const isSectionVisible = sectionTop - windowHalf < 0;
         if (isSectionVisible) section.classList.add("ativo");
+        else if (section.classList.contains("ativo")) {
+          section.classList.remove("ativo");
+        }
       });
     };
     animaScroll();
