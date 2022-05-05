d6fcf57c33738cee37f339365e107b6c06c5567f
Tiago <tiagodev@protonmail.com>
2022-05-05 15:41:08 -0300

    feat: add fetching data from bitcoin and handling errors

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,18 @@
+export default function initFetchBitcoin() {
+  async function fetchBitcoin(url) {
+    try {
+      const dataResponse = await fetch(url);
+      const dataJSON = await dataResponse.json();
+
+      const btcPreco = document.querySelector(".btc-preco");
+      btcPreco.innerText = (1000 / dataJSON.BRL.sell).toFixed(4);
+      console.log(dataJSON.BRL.sell);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  fetchBitcoin("https://blockchain.info/ticker");
+}
+
+// https://blockchain.info/ticker
--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/initFuncionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 initScrollSmooth();
 initAnimationScroll();
@@ -19,3 +20,4 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
+initFetchBitcoin();
